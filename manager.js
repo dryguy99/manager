7ba43582871a76789537b5aaf6e4f1b60d6ea0ac
Timothy Dry <timothy.dry@verizon.net>
2017-04-01 10:07:27 -0400

    added functions to player

--- manager.js
+++ manager.js
@@ -4,17 +4,37 @@ var playerArray = [];
 //-----------------------------------------------------------
 // constructor function used to create programmers objects
 function Player(name, position, offense, defense) {
-  this.name = name;
-  this.position = position;
-  this.offense = offense;
-  this.defense = defense;
-  this.printInfo = function() {
-	console.log("==============================");
-	console.log("");
-	console.log("Name: " + this.name + "\nPosition: " + this.position + "\noffenseValue: " +
-	this.offenseValue + "\ndefenseValue: " + this.defenseValue );
-	console.log("");
-	};
+	this.name = name;
+	this.position = position;
+	this.offense = offense;
+	this.defense = defense;
+	this.printInfo = function() {
+		console.log("==============================");
+		console.log("");
+		console.log("Name: " + this.name + "\nPosition: " + this.position + "\noffenseValue: " +
+		this.offenseValue + "\ndefenseValue: " + this.defenseValue );
+		console.log("");
+		};
+	this.goodGame = function() {
+    if (Math.floor(Math.random() * 2) === 0) {
+      this.offense++;
+      console.log(this.name + "'s offense has gone up!\n----------");
+    }
+    else {
+      this.defense++;
+      console.log(this.name + "'s defense has gone up!\n----------");
+    }
+  };
+  this.badGame = function() {
+    if (Math.floor(Math.random() * 2) === 0) {
+      this.offense--;
+      console.log(this.name + "'s offense has gone down!\n----------");
+    }
+    else {
+      this.defense--;
+      console.log(this.name + "'s defense has gone down!\n----------");
+    }
+  };
 }
 //-----------------------------------------------------------
 
